a129d8f271e7f947d54aa9d2ddabea6bccb1fb8f
Yeicamm <122379855+Yeicamm@users.noreply.github.com>
2024-09-26 18:47:50 -0500

    Microservice configuration eureka

---
+++ scotia/src/main/java/com/eureka/scotia/ScotiaApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.scotia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ScotiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScotiaApplication.class, args);
+    }
+
+}
---
+++ scotia/src/test/java/com/eureka/scotia/ScotiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.scotia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
